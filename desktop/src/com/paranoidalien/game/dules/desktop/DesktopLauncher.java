3daef160493c129c2389e8e0d4d7978d69fc749a
unknown <wilkdavi@WILKDAVI2.americas.hpqcorp.net>
2014-12-06 02:30:24 -0600

    ASDF to move map....test for lines....padded tileset textures

--- desktop/src/com/paranoidalien/game/dules/desktop/DesktopLauncher.java
+++ desktop/src/com/paranoidalien/game/dules/desktop/DesktopLauncher.java
@@ -15,9 +15,8 @@ public class DesktopLauncher {
         config.title = "DULES";
         config.resizable = false;
         config.useGL30 = false;
-        config.width = 1920;
-        config.height = 1080;
-        config.vSyncEnabled = false;
+        config.width = 1280;
+        config.height = 720;
         //config.foregroundFPS = 120;
         config.width = 1280;
         config.height = 720;
