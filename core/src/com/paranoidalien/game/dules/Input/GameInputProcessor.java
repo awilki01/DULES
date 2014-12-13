c38250d6c28345ca6503a232b4bc7c7d9b0cc488
awilki01 <adam.wilkins@mygrande.net>
2014-12-13 16:35:09 -0600

    commit

--- core/src/com/paranoidalien/game/dules/Input/GameInputProcessor.java
+++ core/src/com/paranoidalien/game/dules/Input/GameInputProcessor.java
@@ -3,37 +3,49 @@ package com.paranoidalien.game.dules.Input;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.paranoidalien.game.dules.Entities.Player;
+import com.paranoidalien.game.dules.HUD.Inventory;
 
 /**
  * Created by Adam on 11/24/2014.
  */
 public class GameInputProcessor implements InputProcessor{
-    private OrthographicCamera tileCam, playerCam;
+    private OrthographicCamera tileCam, playerCam, hudCam;
     private Player player;
+    private Inventory inventory;
+    private Vector3 vec3;
+    private Vector2 vec;
 
-    public GameInputProcessor(OrthographicCamera tileCam, OrthographicCamera playerCam, Player player){
+    public GameInputProcessor(OrthographicCamera tileCam, OrthographicCamera playerCam, OrthographicCamera hudCam, Player player, Inventory inventory){
         this.tileCam = tileCam;
         this.playerCam = playerCam;
+        this.hudCam = hudCam;
         this.player = player;
+        this.inventory = inventory;
+
+        vec = new Vector2();
+        vec3 = new Vector3();
     }
 
     @Override
     public boolean keyDown(int keycode) {
-        /*
+
         switch(keycode){
-            // boolean order: left, right, up, down
-            case Input.Keys.DOWN:   player.move(false, false, false, true);
-                                    break;
-            case Input.Keys.UP:     player.move(false, false, true, false);
-                                    break;
-            case Input.Keys.RIGHT:  player.move(false, true, false, false);
-                                    break;
-            case Input.Keys.LEFT:   player.move(true, false, false, false);
-                                    break;
-            default:                break;
+            //case Input.Keys.DOWN:   player.move(false, false, false, true);
+            //                        break;
+            //case Input.Keys.UP:     player.move(false, false, true, false);
+            //                        break;
+            //case Input.Keys.RIGHT:  player.move(false, true, false, false);
+            //                        break;
+            //case Input.Keys.LEFT:   player.move(true, false, false, false);
+             //                       break;
+            case Input.Keys.I:        inventory.toggle();
+                                      break;
+            default:                  break;
         }
-        */
+
         return false;
     }
 
@@ -49,16 +61,33 @@ public class GameInputProcessor implements InputProcessor{
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        vec3.x = screenX;
+        vec3.y = screenY;
+        vec3.z = 0;
+        hudCam.unproject(vec3);
+        vec.x = vec3.x;
+        vec.y = vec3.y;
+        inventory.mouseClick(vec);
+
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        inventory.mouseUp();
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        vec3.x = screenX;
+        vec3.y = screenY;
+        vec3.z = 0;
+        hudCam.unproject(vec3);
+        vec.x = vec3.x;
+        vec.y = vec3.y;
+        inventory.mouseDrag(vec);
+
         return false;
     }
 
